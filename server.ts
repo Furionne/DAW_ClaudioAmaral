19778c79b7f829a0d172bc4af351c227e16920ab
Cláudio Amaral <claudiorafaelamaral@gmail.com>
2022-07-26 12:10:41 +0100

    port adicionado no server

---
+++ server.ts
@@ -0,0 +1,12 @@
+import app from "./app";
+
+const port = 2020;
+
+app.listen(port, () => {
+    console.log(`=========================================`);
+    console.log(`Backend API running on port ${port}...`);
+});
+
+
+
+
