62129c2b243518eee80fc3a205cae7c9fe169082
Cláudio Amaral <claudiorafaelamaral@gmail.com>
2022-07-26 16:52:04 +0100

    atualização cotação schema

--- src/schemas/CotacaoSchema.ts
+++ src/schemas/CotacaoSchema.ts
@@ -5,7 +5,7 @@ const CotacaoSchema = new mongoose.Schema(
   {
     codCotacao: {type: String, required: true, unique: true},
     designation: { type: String, required: true },
-    codTitulo: { type: String, required: true },
+    codCarteira: { type: String, required: true },
     value: { type: Number, required: true },
   },
   {
